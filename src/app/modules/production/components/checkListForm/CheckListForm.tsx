6ec6f4284978f2e500490dbc31f1ecf9d2bf39c0
Ahercode <philipkelly407@gmail.com>
2022-11-13 20:35:08 +0000

    new update

--- src/app/modules/production/components/checkListForm/CheckListForm.tsx
+++ src/app/modules/production/components/checkListForm/CheckListForm.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 import {Field, useFormik} from 'formik'
 import clsx from 'clsx'
 import * as Yup from 'yup'
+import { Divider } from 'antd';
 
 const CheckListForm = () => {
   const [gridData, setGridData] = useState([])
@@ -28,7 +29,7 @@ const CheckListForm = () => {
 
   return (
     <>
-    <div style={{backgroundColor:'white', height:'100vh', padding:'20px', borderRadius:'5px', boxShadow:'2px 2px 15px rgba(0,0,0,0.08)'}}>
+    <div style={{backgroundColor:'white',  padding:'20px', borderRadius:'5px', boxShadow:'2px 2px 15px rgba(0,0,0,0.08)'}}>
       <form id='kt_modal_add_plan_form' className='form' noValidate>
         {/* begin::Scroll */}
         
@@ -39,7 +40,7 @@ const CheckListForm = () => {
           <div className='d-flex justify-content-center mb-7'>
             <span className="fst-itali fs-5 text-danger">Please read each lable carefully and select the appropriate option</span>
           </div>
-         
+          <Divider />
           {/* end::row */}
           <div className='row mb-7'>
             <div className='col-4'>
@@ -212,23 +213,94 @@ const CheckListForm = () => {
                 <option selected>Select one option</option>
                 <option value="1">Ok</option>
                 <option value="2">Repair</option>
+              </select>
+            </div>
+          </div>
+          {/* end::row */}
+          <div className='row mb-7'>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Replace Fuel Filter (ORS) </label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
                 
               </select>
             </div>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Replace Engine oil filter</label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
+              </select>
+            </div>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Remove & clean starter silenser</label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
+              </select>
+            </div>
+          </div>
+          {/* end::row */}
+          <div className='row mb-7'>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Inspect pulleys for cracks & dirt build-up  </label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
+                
+              </select>
+            </div>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Inspect Fuel lines for leaks & damage</label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
+              </select>
+            </div>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Inspect Exhaust manifolds & lines for leaks</label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
+              </select>
+            </div>
+          </div>
+          {/* end::row */}
+          <div className='row mb-10 fw-bolder'>
+            <div className='col-4'>
+              <label className='required fw-bold fs-6 mb-2'>Inspect Air induction system clamps & hoses</label>
+              <select className="form-select form-control form-control-solid mb-3" >
+                <option selected>Select one option</option>
+                <option value="1">Ok</option>
+                <option value="2">Repair</option>
+              </select>
+            </div>
+          </div>
+          <Divider dashed />
+          <div className='row mb-5'>
+            <div className='mb-4'>
+              <input type="checkbox" id="agree" onChange={checkboxHandler} />
+              <label htmlFor="agree"> I have completed <b>the above maintenance and inspections</b></label>
+            </div>
+            <div>
+              <button 
+                disabled={!agree}
+                className='btn btn-primary'
+                data-kt-plans-modal-action='submit' 
+                onClick={btnHandler}>
+                Continue
+              </button>
+            </div>
+             
             
           </div>
-          {/* <div className='row mb-7'>
-            <input type="checkbox" id="agree" onChange={checkboxHandler} />
-            <label htmlFor="agree"> I have completed <b>the above maintenance and inspections</b></label>
-             
-            <button 
-              disabled={!agree}
-              className='btn btn-primary'
-              data-kt-plans-modal-action='submit' 
-              onClick={btnHandler}>
-              Continue
-            </button>
-          </div> */}
       
       </form>
       </div>
