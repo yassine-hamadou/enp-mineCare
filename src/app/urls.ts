ddf32a88f4a3900b8999f085ba3453f29584af14
Yassine Hamadou <yassinehamadou1@gmail.com>
2022-11-18 19:23:51 +0000

    Merge

--- src/app/urls.ts
+++ src/app/urls.ts
@@ -2,5 +2,5 @@
  Use this file to define your base URLs whether on localhost or on the ENP server
  */
 // export const ENP_URL = 'http://localhost:3001'
-// export const ENP_URL = 'http://208.117.44.15/SmWebApi/api'
-export const ENP_URL = 'http://localhost:4192'
+export const ENP_URL = 'http://208.117.44.15/SmWebApi/api'
+// export const ENP_URL = 'http://localhost:4192'
