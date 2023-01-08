3894c8f2d3455616080691129679f8d85471b4d4
Yassine Hamadou <yassinehamadou1@gmail.com>
2023-01-08 23:13:12 +0000

    Making dynamic tabs based on the service types

--- src/app/modules/production/components/checkListForm/Tabs.tsx
+++ src/app/modules/production/components/checkListForm/Tabs.tsx
@@ -5,34 +5,7 @@ import {CheckListForm2} from './CheckListForm2'
 import {CheckListForm3} from './CheckListForm3'
 import {CheckListForm5} from './CheckListForm5'
 import { useParams } from "react-router-dom";
-import axios from "axios";
-import { useQuery } from "react-query";
-import { ENP_URL } from "../../../../urls";
-
-// //call to the api to get the service type
-// const { data: serviceType }: any = useQuery("serviceType", () => {
-//   return axios.get(`${ENP_URL}/Services`);
-// } );
-
-
-const tabList = [
-  {
-    key: 'tab1',
-    tab: String("Section '1' - ENGINE").toUpperCase(),
-  },
-  {
-    key: 'tab2',
-    tab: String("Section '2' - TRANSMISSION/HYDRAULIC/AXLES").toUpperCase(),
-  },
-  {
-    key: 'tab3',
-    tab: String("Section '3' - BODY/FRAME").toUpperCase(),
-  },
-  {
-    key: 'tab4',
-    tab: String("SECTION '5' - OPERATION/CAP").toUpperCase(),
-  },
-]
+import { useQueryClient } from "react-query";
 
 const contentList: Record<string, React.ReactNode> = {
   tab1: (
@@ -58,19 +31,24 @@ const contentList: Record<string, React.ReactNode> = {
 }
 
 const TabsTest: React.FC = () => {
-  //request
-  const {data: services}: any = useQuery('services', () => {
-    return axios.get(`${ENP_URL}/Sections`)
-  })
-  //generate the tabs dynamically
-  const tabs = services?.data.map((service: any) => {
+  //Get the service type with useQueryClient
+  const AllServiceTypes: any = useQueryClient().getQueryData("serviceType");
+  console.log("serviceType", AllServiceTypes);
+  //Get the service type id from the url
+  const params = useParams()
+  const serviceId: any = params.serviceId
+  //Get the service type name from the service type id
+  const serviceType = AllServiceTypes?.data.find((s: any) => s.id === parseInt(serviceId))
+  const sections = serviceType?.sections
+
+  const tabList = sections?.map((s: any, index: any) => {
     return {
-      title: service.name,
-      content: <CheckListForm2 />,
+      key: `tab${index + 1}`,
+      tab: String(`${s.name}`).toUpperCase(),
     }
   })
-  const params = useParams()
-  const serviceId = params.serviceId
+
+
   const [activeTabKey1, setActiveTabKey1] = useState<string>('tab1')
 
   const onTab1Change = (key: string) => {
